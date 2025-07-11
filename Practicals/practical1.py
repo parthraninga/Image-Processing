dccc1d548759ced8abc9d782777a6e559b5c0ca7
parthraninga <23airan041@ldce.ac.in>
2025-07-11 14:53:45 +0530

    feat: practical1

--- Practicals/practical1.py
+++ Practicals/practical1.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 
 import warnings
 warnings.simplefilter('ignore')
@@ -7,7 +7,12 @@ warnings.simplefilter('ignore')
 image1_gray = cv2.imread("Resources/image/demo-image.jpg",cv2.IMREAD_GRAYSCALE)
 image1_color = cv2.imread("Resources/image/demo-image.jpg",cv2.IMREAD_COLOR)
 
+print(image1_gray.shape)
+
 cv2.imshow("gray image",image1_gray)
 cv2.imshow("color image",image1_color)
 
+blank=np.zeros([500,375],dtype='uint8')
+
+cv2.imshow("blank image",blank)
 cv2.waitKey(0)
